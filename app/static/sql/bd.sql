5f5795e0fb2c805dda8c44c08f85f9fe3cabc88f
hechicero <yurihuallpavargas@gmail.com>
2021-01-12 22:25:59 -0500

    fix bd.sql

--- app/static/sql/bd.sql
+++ app/static/sql/bd.sql
@@ -13,19 +13,19 @@ CREATE TABLE IF NOT EXISTS works (
 CREATE TABLE IF NOT EXISTS researchs(
 	research_id uuid DEFAULT uuid_generate_v4 (),
 	title varchar(120),
-	editorial varchar(120),
-	abstract varchar(240),
-	link varchar(240),
-	github varchar(240),
+	editorial varchar(30),
+	abstract varchar(340),
+	link varchar(120),
+	github varchar(120),
 	state boolean,
 	primary key(research_id)
 );
 
 CREATE TABLE IF NOT EXISTS info_init(
 	init_id uuid DEFAULT uuid_generate_v4 (),
-	greeting varchar(120),
-	name varchar(120),
-	phrase varchar(120),
+	greeting varchar(30),
+	name varchar(30),
+	phrase varchar(60),
 	introduce varchar(240),
 	email_link varchar(240),
 	state boolean,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS info_init(
 
 CREATE TABLE IF NOT EXISTS info_me(
 	me_id uuid DEFAULT uuid_generate_v4 (),
-	greeting varchar(120),
+	greeting varchar(240),
 	content varchar(240),
 	goal varchar(240),
 	finish varchar(120),
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS info_me(
 
 CREATE TABLE IF NOT EXISTS skills(
 	skill_id serial,
-	name varchar(120),
+	name varchar(30),
 	me_id uuid,
 	primary key(skill_id),
 	foreign key(me_id) references info_me(me_id)
@@ -53,10 +53,10 @@ CREATE TABLE IF NOT EXISTS skills(
 
 CREATE TABLE IF NOT EXISTS me(
 	me_id serial,
-	name varchar(120),
-	last_name varchar(120),
-	email varchar(120),
-	cv varchar(240),
+	name varchar(30),
+	last_name varchar(30),
+	email varchar(60),
+	cv varchar(120),
 	facebook varchar(120),
 	linkedin varchar(120),
 	github varchar(120),
